a16e1bd389cb911c17bdf7a0453788e3b6a13370
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2024-09-30 12:09:16 +0600

    Added main page

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,9 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
+import Main from "../pages/Main/Main";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Main></Main>,
   },
 ]);
 
